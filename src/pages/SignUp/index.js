4c23a54501c18e0ad927021b37f385061553fb12
Joao Lucas <jlmelosouza18@gmail.com>
2020-02-01 09:50:19 -0300

    feat: page SignUp

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,14 +1,52 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
 
-// import { Container } from './styles';
-
+import logo from '~/assets/logo.png';
 import Background from '~/components/Background';
 
-export default function SignUp() {
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignUp({ navigation }) {
   return (
     <Background>
-      <Text>SignUp</Text>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCapitalaze="words"
+            placeholder="Nome Completo"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalaze="none"
+            placeholder="Digite seu melhor e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            sucureTextEntry
+            placeholder="Sua senha secreta"
+          />
+
+          <SubmitButton onPress={() => {}}>Criar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Já tenho conta</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
